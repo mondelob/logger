dc42caf28b2b5d020bc99394bad1c470242be76a
Bruno Mondelo <mondelob14@gmail.com>
2017-11-30 12:25:08 +0100

    Added Write: sync and async

--- lib/yalog.c
+++ lib/yalog.c
@@ -1,10 +1,35 @@
-/* logger - Simple message logger
+/* yalog - Simple message logger
  * This file is under the MIT license
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Bruno Mondelo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include<pthread.h>
+#include <semaphore.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,12 +37,26 @@
 
 #include "yalog.h"
 
+struct _threads {
+  int t_num;          /* Number of threads created */
+  int t_len;          /* Actual lenght of the thread array */
+  pthread_t * t_tid;  /* Array of threads */
+};
+/* Structure of the subthreads on a log */
 
 struct _log {
-  int l_fd;       /* The file descriptor to write */
-  int l_isfile;   /* Set when file is openned by yalog */
+  int l_fd;                 /* The file descriptor to write */
+  int l_isfile;             /* Set when file is openned by yalog */
+  sem_t * l_write;          /* Write semaphore */
+  struct _threads * l_tid;  /* Set of subthreads */
 };
 /* Structure for the logs */
+
+struct _args {
+  struct _log * a_l;      /* Log to write */
+  const char * a_buf;     /* Buffer to write*/
+};
+/* Structure for the write log function */
 
 static int _num = 0;
 /* The number of logs created */
@@ -27,6 +66,46 @@ static int _len = 0;
 
 static struct _log ** _logs;
 /* Array containing all log pointers */
+
+int
+_create_log(struct _log * l, int fd, int isfile) {
+  sem_t * s;
+  struct _threads * tid;
+
+  l->l_fd = fd;
+  l->l_isfile = isfile;
+
+  s = malloc(sizeof(sem_t));
+  if (sem_init(s, 0, 1) < 0) {
+    perror("_create_log : error creating semaphore");
+    return -1;
+  }
+  l->l_write = s;
+
+  tid = malloc(sizeof(struct _threads));
+  tid->t_num = 0;
+  tid->t_len = 0;
+  l->l_tid = tid;
+
+  return 0;
+}
+
+int
+_create_thread(struct _log * l) {
+  if (l->l_tid->t_num == 0) {
+    l->l_tid->t_tid = malloc(sizeof(pthread_t));
+    l->l_tid->t_len++;
+  }
+  else if (l->l_tid->t_num == l->l_tid->t_len) {
+    l->l_tid->t_len = l->l_tid->t_len * 2;
+    l->l_tid->t_tid = realloc(l->l_tid->t_tid, sizeof(pthread_t) * l->l_tid->t_len);
+    for (int i = l->l_tid->t_num + 1; i < l->l_tid->t_len; i++) {
+      l->l_tid->t_tid[i] = -1;
+    }
+  }
+  return l->l_tid->t_num++;
+}
+/* Creates a thread and assigns the thread to the list */
 
 int
 _add_log(struct _log * l) {
@@ -46,6 +125,23 @@ _add_log(struct _log * l) {
 }
 /* Adds a log on the array */
 
+struct _log *
+_get_log(int numlog) {
+  if (numlog >= _num) {
+    errno = EFAULT;
+    perror("yalog_close : index out of range");
+    return NULL;
+  }
+
+  if (_logs[numlog] == NULL) {
+    errno = EIO;
+    perror("yalog_close : log already closed");
+    return NULL;
+  }
+
+  return _logs[numlog];
+}
+
 int
 _open_file(const char * filename) {
   int fd;
@@ -58,16 +154,17 @@ _open_file(const char * filename) {
 /* Opens a file and returns the file descriptor */
 
 int
-_writefd(int fd, const void * buf, size_t count) {
+_write_descriptor(int fd, const void * buf, size_t count) {
   ssize_t bytes;
 
   bytes = write(fd, buf, count);
 
   if (bytes < 0) {
     if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN) {
-      return _logger_write_descriptor(fd, buf + bytes, count - bytes, err);
+      return _write_descriptor(fd, buf + bytes, count - bytes);
     }
     else {
+      perror("_write_descriptor : error writing");
       return -1;
     }
   }
@@ -75,6 +172,35 @@ _writefd(int fd, const void * buf, size_t count) {
   return 0;
 }
 /* Writes all bytes to a file descriptor */
+
+int
+_write_log(struct _args * argv) {
+  struct _log * l = argv->a_l;
+  const void * buf = argv->a_buf;
+  free(argv);
+
+  ssize_t size;
+
+  if (sem_wait(l->l_write) != 0) {
+    perror("_write_log : error waiting for semaphore");
+    return -1;
+  }
+
+  size = (ssize_t)strlen(buf);
+
+  if (_write_descriptor(l->l_fd, buf, size) < 0) {
+    perror("_write_log : error writing to descriptor");
+    return -2;
+  }
+
+  if (sem_post(l->l_write) != 0) {
+    perror("_write_log : error posting semaphore");
+    return -3;
+  }
+
+  return 0;
+}
+/* Writes to a log */
 
 int
 yalogfn(char * filename) {
@@ -85,9 +211,13 @@ yalogfn(char * filename) {
     perror("yalogfn : error opening file descriptor");
     return -1;
   }
+
   l = malloc(sizeof(struct _log));
-  l->l_fd = fd;
-  l->l_isfile = 1;
+  if (_create_log(l, fd, 1) < 0) {
+    perror("yalogfn : error creating log");
+    return -2;
+  }
+
   index = _add_log(l);
 
   return index;
@@ -100,8 +230,11 @@ yalogfd(int fd) {
   int index;
 
   l = malloc(sizeof(struct _log));
-  l->l_fd = fd;
-  l->l_isfile = 0;
+  if (_create_log(l, fd, 1) < 0) {
+    perror("yalogfn : error creating log");
+    return -2;
+  }
+
   index = _add_log(l);
 
   return index;
@@ -109,31 +242,94 @@ yalogfd(int fd) {
 /* Initializes an instance of the logger with an open file descriptor */
 
 int
-yalog_close(int numlog) {
-  int retval;
+yalog_sync_write(int numlog, const char * buf) {
+  struct _log * l;
+  struct _args * argv;
 
-  if (numlog >= _num) {
-    errno = EFAULT;
-    perror("yalog_close : index out of range");
+  if ((l = _get_log(numlog)) == NULL) {
+    perror("yalog_sync_write : error got log");
     return -1;
   }
 
-  if (_logs[numlog] == NULL) {
-    errno = EIO;
-    perror("yalog_close : log already closed");
+  argv = malloc(sizeof(struct _args));
+  argv->a_l = l;
+  argv->a_buf = buf;
+
+  if (_write_log(argv) < 0) {
+    free(argv);
+    perror("yalog_sync_write : error writing log");
+    return -1;
+  }
+
+  argv = NULL;
+
+  return 0;
+}
+/* Writes to log sync */
+
+int
+yalog_async_write(int numlog, const char * buf) {
+  struct _log * l;
+  struct _args * argv;
+  int index;
+
+  if ((l = _get_log(numlog)) == NULL) {
+    perror("yalog_async_write : error got log");
+    return -1;
+  }
+
+  argv = malloc(sizeof(struct _args));
+  argv->a_l = l;
+  argv->a_buf = buf;
+
+  index = _create_thread(l);
+  if (pthread_create(&(l->l_tid->t_tid[index]), NULL, (void *)&_write_log, argv) != 0) {
+    free(argv);
+    perror("yalog_async_write : error creating thread");
     return -2;
+  }
+  argv = NULL;
+
+  return 0;
+}
+/* Writes to log async */
+
+int
+yalog_close(int numlog) {
+  int retval;
+  struct _log * l;
+
+  if ((l = _get_log(numlog)) == NULL) {
+    perror("yalog_close : error got log");
+    return -1;
   }
 
   retval = 0;
-  if (_logs[numlog]->l_isfile) {
-    if (close(_logs[numlog]->l_fd) != 0) {
+
+  for (int i = 0; i < l->l_tid->t_num; i++) {
+    if (l->l_tid->t_tid[i] != -1) {
+      pthread_join(l->l_tid->t_tid[i], NULL);
+    }
+  }
+  free(l->l_tid->t_tid);
+  free(l->l_tid);
+  l->l_tid = NULL;
+
+  if (sem_destroy(l->l_write) < 0) {
+    perror("yalog_close : error destroying semaphore");
+    retval = -3;
+  }
+  free(l->l_write);
+
+  if (l->l_isfile) {
+    if (close(l->l_fd) != 0) {
       perror("yalog_close : error closing file");
-      retval = -3;
+      retval = -4;
     }
   }
 
-  free(_logs[numlog]);
-  _logs[numlog] = NULL;
+  free(l);
+  l = NULL;
 
   return retval;
 }
--- lib/yalog.h
+++ lib/yalog.h
@@ -1,5 +1,27 @@
-/* logger - Simple message logger
+/* yalog - Simple message logger
  * This file is under the MIT license
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Bruno Mondelo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef YALOG_H
@@ -10,6 +32,12 @@ int yalogfn(char * filename);
 
 int yalogfd(int fd);
 /* Initializes an instance of the logger with an open file descriptor */
+
+int yalog_sync_write(int numlog, const char * buf);
+/* Writes to log sync */
+
+int yalog_async_write(int numlog, const char * buf);
+/* Writes to log async */
 
 int yalog_close(int numlog);
 /* Closes a single log */
